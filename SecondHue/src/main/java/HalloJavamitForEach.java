ae41ad1af88cbd4aec57a4e8d1e64d94cf6a1936
Elias <Reiter.Elias@sus.htl-grieskirchen.at>
2020-09-28 13:22:04 +0200

    first commit

---
+++ SecondHue/src/main/java/HalloJavamitForEach.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class HalloJavamitForEach {
+    public List<String> loop = new ArrayList();
+
+    public void forEach(){
+        for (String s :
+                loop) {
+            System.out.println(s);
+        }
+    }
+
+    public void forEachMethod(){
+        loop.forEach();
+    }
+
+}
