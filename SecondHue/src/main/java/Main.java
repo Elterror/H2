b2bf9c3cf33d2a9be712f1591c048ee57bf17895
Elias <Reiter.Elias@sus.htl-grieskirchen.at>
2020-09-28 14:56:32 +0200

    setTester read File

--- SecondHue/src/main/java/Main.java
+++ SecondHue/src/main/java/Main.java
@@ -1,11 +1,43 @@
 import java.io.File;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         NumberTester t = new NumberTester("Data");
 
-        NumberTest oddTester = (number) -> (number % 2) == 0;
-        t.setOddEvenTester(oddTester);
+        t.setOddEvenTester((number) -> (number % 2) == 0);
+
+        t.setPrimeTester((number) -> {
+            if (number <= 1) {
+                return false;
+            }
+
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        });
+
+        t.setPrimeTester((number) -> {
+            int palindrome = number; // copied number into variable
+            int reverse = 0;
+
+            while (palindrome != 0) {
+                int remainder = palindrome % 10;
+                reverse = reverse * 10 + remainder;
+                palindrome = palindrome / 10;
+            }
+
+            // if original and reverse of number is equal means
+            // number is palindrome in Java
+            if (number == reverse) {
+                return true;
+            }
+            return false;
+
+
+        });
 
 
     }
--- SecondHue/src/main/java/NumberTester.java
+++ SecondHue/src/main/java/NumberTester.java
@@ -1,23 +1,53 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class NumberTester {
-    public NumberTester (String fileName ) {}
+    public NumberTester(String fileName) {
+
+        Map<Integer, Integer> numbers = new HashMap<>();
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(new File(fileName)));
+            String line = br.readLine();
+            int numberOfTests = Integer.parseInt(line);
+            line = br.readLine();
+            while (line != null) {
+                String[] spliter = line.split(" ");
+                numbers.put(Integer.parseInt(spliter[1]), Integer.parseInt(spliter[0]));
+                line = br.readLine();
+            }
+            br.close();
+        } catch (IOException ex) {
+            Logger.getLogger(NumberTester.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
 
     NumberTest oddTester;
     NumberTest palindromeTester;
     NumberTest primeTester;
 
 
-    public void setOddEvenTester (NumberTest oddTester ) {
+    public void setOddEvenTester(NumberTest oddTester) {
         this.oddTester = oddTester;
     }
-    public void setPrimeTester(NumberTest primeTester){
+
+    public void setPrimeTester(NumberTest primeTester) {
         this.primeTester = primeTester;
     }
-    public void setPalindromeTester(NumberTest palindromeTester){
+
+    public void setPalindromeTester(NumberTest palindromeTester) {
         this.palindromeTester = palindromeTester;
     }
 
-    public void testFile(){
-        oddTester.testNumber(4);
+    public void testFile() {
+        
     }
 
 }
